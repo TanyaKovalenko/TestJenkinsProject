bf545e3f4cdc1110df5f4a06b728cc790125796a
TanyaKovalenko <tanyakovalenko1994@gmail.com>
2016-10-28 01:23:20 +0300

    Update startBuild.bat

--- startBuild.bat
+++ startBuild.bat
@@ -1,2 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat"
-"C:\Make\make.exe"
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat" && "C:\Make\make.exe"
